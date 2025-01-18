84c0b4d2e5419b1cd3b888903d8036f8e17fda2d
Darklte732 <santos.joseph1998@gmail.com>
2025-01-18 01:35:59 -0500

    Updated initial button text to 'Download Free Guide'

--- components/GuideDownloadForm.tsx
+++ components/GuideDownloadForm.tsx
@@ -178,7 +178,7 @@ export function GuideDownloadForm() {
               Processing...
             </>
           ) : (
-            'Submit Form'
+            'Download Free Guide'
           )}
         </Button>
       ) : (
