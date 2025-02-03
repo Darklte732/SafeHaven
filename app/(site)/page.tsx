393e14f8f77940dfae2d94fd9ae52cf616aef17e
Your Name <your.email@example.com>
2025-02-03 12:51:59 -0500

    Fix build issues by restructuring app directory and updating dependencies

--- app/(site)/page.tsx
+++
@@ -1,9 +0,0 @@
-import HomePageWrapper from '@/components/wrappers/HomePageWrapper'
-
-export default function HomePage() {
-  return (
-    <main className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900">
-      <HomePageWrapper />
-    </main>
-  )
-} 
